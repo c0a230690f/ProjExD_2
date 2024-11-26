ce6450ec7f116cbbada9a49857d4cadb11f35931
SMS <c0a230690f@edu.teu.ac.jp>
2024-11-26 15:09:13 +0900

    練習４ゲームオーバー

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -12,20 +13,43 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect) -> tuple[bool,bool]:
+    """
+    引数であたえらrectが画面の中が外かを判定する
+    引数:こうかとんrector 爆弾rect
+    戻り値:真理値ダブル(横、縦)/画面内:Ture,画面外:False
+    
+    """
+    yoko,tate = True,True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko,tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
-    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 1.0)
+    kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20,20)) #爆弾用にの空surface
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10) #爆弾円を描く
+    bb_img.set_colorkey((0,0,0)) #
+    bb_rct = bb_img.get_rect() #爆弾rectの抽出
+    bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    vx,vy = 5,5
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):
+            print("ゲームオーバー")
+            return #ゲームオーバー
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -36,7 +60,17 @@ def main():
                 sum_mv[1] += tpl[1]
 
         kk_rct.move_ip(sum_mv)
+        #こうかとんが画面外なら元の場所に戻す
+        if check_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx,vy) #爆弾動く
+        yoko,tate = check_bound(bb_rct)
+        if not yoko:
+            vx*= -1
+        if not tate:
+            vy *= -1
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
